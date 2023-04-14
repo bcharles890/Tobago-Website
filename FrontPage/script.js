0d6b6c0bac07343cd68dbbd97534822fef370dd9
Brandon Charles <bcharlesit890@gmail.com>
2023-04-14 12:56:41 -0400

    added javascript logic to create a hiding navbar added javascript logic to create a hiding navbar if the page is scrolled the navbar appears

--- FrontPage/script.js
+++ FrontPage/script.js
@@ -1,9 +1,9 @@
-const buttons = document.querySelectorAll("[data-carosel-button]")
+const buttons = document.querySelectorAll("[data-carosel-button]") //function used to selected the data carosel button
 
 
 buttons.forEach(button => {
-    button.addEventListener("click", ()=> {
-       const offset = button.dataset.caroselButton === "next" ? 1: -1
+    button.addEventListener("click", ()=> {                        
+       const offset = button.dataset.caroselButton === "next" ? 1: -1  // when next is clicked it will either go up in the carosel index by 1 or decrese by 1
        const slides = button
        .closest("[data-carosel]")
        .querySelector("[data-slides]")
@@ -17,4 +17,14 @@ buttons.forEach(button => {
        delete activeSlide.dataset.active
 
     })
-})
+}) // carosel code ends here
+
+window.onscroll = function() {scrollFunction()}; // this code is for the navbar that appears one the user scrolls
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){  //if the document is scrolled then the navbar appears 
+        document.getElementById("navbar").style.top = "0"; //if the navbar is at the top of the page then it is hidden
+    }
+}
+
+
